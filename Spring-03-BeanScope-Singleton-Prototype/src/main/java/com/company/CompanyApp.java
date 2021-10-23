316b089b659a74d5a2915d8b9a2156e0e491b392
Ali <92939861+cy-ali@users.noreply.github.com>
2021-10-23 13:22:28 +0300

    Bean-Scope files added

---
+++ Spring-03-BeanScope-Singleton-Prototype/src/main/java/com/company/CompanyApp.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import com.company.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CompanyApp {
+    public static void main(String[] args) {
+        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        Course course1 = container.getBean("java", Course.class);
+
+        Course course2 = container.getBean("java", Course.class);
+
+        System.out.println("Pointing to the same object: " + (course1 == course2));
+
+        System.out.println("Memory location for the course1 " + course1);
+        System.out.println("Memory location for the course2 " + course2);
+
+    }
+}
