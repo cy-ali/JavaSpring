88d83cf8e3b81cf1a00cd7aa43e91d4c848b2515
Ali <92939861+cy-ali@users.noreply.github.com>
2021-10-28 19:12:49 +0300

    JavaAnnotations-Value files added

---
+++ Spring-05-JavaAnnotations-Value/src/main/java/com/company/services/Java.java
@@ -0,0 +1,44 @@
+package com.company.services;
+
+import com.company.interfaces.Course;
+import com.company.interfaces.ExtraSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class Java implements Course {
+
+    @Value("JD1")
+    private String batch;
+
+    @Value("${instructor}")
+    private String instructor;
+
+    @Value("${days}")
+    private String[] days;
+
+    private ExtraSession extraSession;
+
+    @Override
+    public String toString() {
+        return "Java{" +
+                "batch='" + batch + '\'' +
+                ", instructor='" + instructor + '\'' +
+                ", days=" + Arrays.toString(days) +
+                '}';
+    }
+
+    @Autowired
+    public Java(@Qualifier("officeHours") ExtraSession extraSession) {
+        this.extraSession = extraSession;
+    }
+
+    @Override
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours : " + (40 + extraSession.getHours()));
+    }
+}
