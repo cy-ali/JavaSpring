25ccb3eb6511ae6cb465cc0326930441122a7e88
Ali <92939861+cy-ali@users.noreply.github.com>
2021-11-20 19:12:30 +0300

    SQL Review

--- Spring-08-SpringBoot-MVC-Form/src/main/java/com/company/model/Employee.java
+++ Spring-08-SpringBoot-MVC-Form/src/main/java/com/company/model/Employee.java
@@ -2,10 +2,6 @@ package com.company.model;
 
 import lombok.*;
 
-import java.time.LocalDate;
-import java.time.Period;
-import java.time.format.DateTimeFormatter;
-
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
