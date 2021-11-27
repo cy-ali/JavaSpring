5519502d8f0537163efdd7c411bfc97917dbba80
Ali <92939861+cy-ali@users.noreply.github.com>
2021-11-27 12:52:05 +0300

    Spring ORM - OneToMany

---
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/company/entity/Address.java
@@ -0,0 +1,28 @@
+package com.company.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String street;
+    private String zipCode;
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Person person;
+
+
+    public Address(String street, String zipCode) {
+        this.street = street;
+        this.zipCode = zipCode;
+    }
+}
---
+++ Spring-09-ORM-Mapping-OneToMany/src/main/java/com/company/entity/Person.java
@@ -0,0 +1,33 @@
+package com.company.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    @OneToMany(mappedBy = "person")
+    private List<Address> addresses;
+
+//    @OneToMany(cascade = CascadeType.ALL)
+//    @JoinColumn(name = "person_id")
+//    private List<Address> addresses;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
