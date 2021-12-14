d7af748a3d0fbaf7df81de345970bd37a83f0d87
Ali <92939861+cy-ali@users.noreply.github.com>
2021-12-14 12:22:01 +0300

    Spring Security - Custom Configuration

---
+++ Spring-10-Security-CustomConfiguration/src/main/java/com/company/Spring10SecurityCustomConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10SecurityCustomConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring10SecurityCustomConfigurationApplication.class, args);
+    }
+
+}
