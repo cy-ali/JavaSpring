a5a79185a7c87c01ced96f67a97efe48ef414a34
Ali <92939861+cy-ali@users.noreply.github.com>
2021-12-16 22:48:57 +0300

    Spring Security - Form-Auth

---
+++ Spring-10-Security-FormAuth/src/main/java/com/company/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.company.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/index")
+    public String index() {
+
+        return "/index";
+    }
+
+    @GetMapping(value = {"/login", "/"})
+    public String login() {
+
+        return "login";
+    }
+}
